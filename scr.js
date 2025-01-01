52a86f8dab7790651732a8635c1e282e11043c85
zamit00 <zamit00@gmail.com>
2025-01-01 10:15:10 +0200

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,26 +1,68 @@
 // אנימציה כאשר הגולל מגיע לחלקים שונים של האתר
-window.addEventListener('scroll', function() {
-    const services = document.querySelectorAll('.service');
-    const testimonials = document.querySelectorAll('.testimonial');
+function chng(x){
+
+    x.classList.toggle("change");
+    var btn=document.getElementsByClassName('ser');
+    Array.from(btn).forEach(element=>{
+      if(element.className==="ser"){element.className="ser sera"}
+      else{element.className="ser"}
+     
+      
+  });
+  var allDropdowns = document.querySelectorAll('.dropdown-content');
+  allDropdowns.forEach(function(dropdown) {dropdown.style.display = "none";});
+  }
+
+  function taf(button) {
+
+    var allDropdowns = document.querySelectorAll('.dropdown-content');
+    allDropdowns.forEach(function(dropdown) {
+      if (dropdown !== button.nextElementSibling) {
+        dropdown.style.display = "none";
+        
+      }
+    });
+  
+    var dropdownContent = button.nextElementSibling;
+    if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
+      
+      dropdownContent.style.display = "block";  
+    } else {
+      dropdownContent.style.display = "none";   
+    }
+    if(window.innerWidth>768){  hover();}
     
-    services.forEach(service => {
-        if (isInViewport(service)) {
-            service.classList.add('visible');
-        } else {
-            service.classList.remove('visible');
-        }
-    });
+  }
 
-    testimonials.forEach(testimonial => {
-        if (isInViewport(testimonial)) {
-            testimonial.classList.add('visible');
-        } else {
-            testimonial.classList.remove('visible');
-        }
-    });
-});
+  function hisht(x) {
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return rect.top >= 0 && rect.bottom <= window.innerHeight;
-}
+    const screenw=window.innerWidth;
+    const screenh=window.innerHeight;
+    const maxw=Math.min(screenw*0.95,800);
+    const maxh=Math.min(screenh*0.95,600);
+    const windowf=`width=${maxw},height=${maxh},resizable=yes,scrollbars=yes`;
+    
+    // פתיחת הקובץ בחלון חדש
+    window.open( x, '_blank',  windowf
+    );
+  
+    // סגירת תפריט ההמבורגר
+    var hamburgerMenu = document.getElementById("tafrit");
+    if (hamburgerMenu.classList.contains("change")) {
+      hamburgerMenu.classList.remove("change");
+    }
+  
+    // החזרת הכפתורים למצבם הרגיל
+    var btns = document.getElementsByClassName('btn');
+    Array.from(btns).forEach(function(btn) {
+      if (btn.classList.contains("btna")) {
+        btn.classList.remove("btna");
+      }
+    });
+  
+    // הסתרת כל התפריטים הפתוחים
+    var allDropdowns = document.querySelectorAll('.dropdown-content');
+    allDropdowns.forEach(function(dropdown) {
+      dropdown.style.display = "none";
+    });
+  }
